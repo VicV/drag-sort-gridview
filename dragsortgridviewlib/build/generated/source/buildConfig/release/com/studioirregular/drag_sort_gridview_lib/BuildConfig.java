30478e1d22b8c0936fe53456a72b3e2b6e5df72c
VicV <victorvuc@gmail.com>
2014-11-02 14:15:02 -0800

    Moved to Gradle. Removed manual array copies.
    Also moved to SDK 21 with sdk checks for lower SDKs so Lint doesn't
    break.

---
+++ dragsortgridviewlib/build/generated/source/buildConfig/release/com/studioirregular/drag_sort_gridview_lib/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.studioirregular.drag_sort_gridview_lib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.studioirregular.drag_sort_gridview_lib";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
